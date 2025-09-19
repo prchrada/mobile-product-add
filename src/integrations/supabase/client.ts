1ed3df7cf31ff614022c5f4b89215e7e3e01ec63
flarious <77673448+flarious@users.noreply.github.com>
2025-09-19 12:46:18 +0700

    ใช้ env ในการเก็บ key Supabase

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,13 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://dilsjiojwuvrrthisper.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpbHNqaW9qd3V2cnJ0aGlzcGVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NDE2NjIsImV4cCI6MjA3MzAxNzY2Mn0.auIWZ9O6ZMeeCBieci5wiNg_d3xrmKvSBWOhv8X8cIo";
-
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
